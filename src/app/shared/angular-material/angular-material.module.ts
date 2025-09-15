f8b195e23ec1ff4d37f3fe02f83049b66fdccbb9
Angel Montes <angelmontes963@gmail.com>
2025-09-14 21:39:31 -0600

    feat: mejorar estilos y estructura de componentes, optimizar diseño responsivo

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 
+
 @NgModule({
   declarations: [],
   imports: [
